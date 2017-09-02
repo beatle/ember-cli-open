c4dc8d9d9c03564321fdda99bfaf48b2d135dd4c
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-09-03 00:40:39 +0300

    test: generate test target app

--- tests/acceptance-test.js
+++ tests/acceptance-test.js
@@ -1,10 +1,158 @@
 'use strict';
 
-var targetApp = 'tests/dummy';
 var fixturify = require('fixturify');
 var execa = require('execa');
 
-let envConfigContent = function(overrides) {
+var Promise = require("rsvp").Promise;
+var EOL = require("os").eol;
+var path = require('path');
+var fs = require('fs');
+var tmp = require('tmp');
+
+var chai = require('chai');
+
+var expect = chai.expect;
+
+var runCommand = require('ember-cli-internal-test-helpers/lib/helpers/run-command');
+
+var TIME_TO_WAIT_FOR_BUILD = 10000;
+var TIME_TO_WAIT_FOR_STARTUP = 10000;
+
+var _log = [];
+
+// var targetApp = 'tests/dummy';
+var targetApp = 'dummy';
+var origWorkingDir = process.cwd();
+
+describe('commands/serve', function () {
+  before(function() {
+
+    this.timeout(300000);
+    let tmpDir = tmp.dirSync();
+
+    process.chdir(tmpDir.name);
+
+    return execa('ember', [
+      'new', targetApp,
+      '--skip-npm',
+      `--directory=${tmpDir.name}`
+    ], {
+      localDir: path.join(origWorkingDir, 'node_modules', '.bin')
+    }).then(() => {
+      const manifest = JSON.parse(fs.readFileSync('package.json', 'UTF-8'));
+      manifest.devDependencies["ember-cli-open"] = `file:${ origWorkingDir }`;
+      fs.writeFileSync('package.json', JSON.stringify(manifest));
+      fs.writeFileSync('ember-cli-build.js', `
+const fs = require('fs');
+const EOL = require('os').EOL;
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    open: {
+      command(urlToOpen) {
+        fs.appendFileSync('fake-opener',  urlToOpen + EOL, {
+          flag: 'a'
+        });
+      }
+    }
+  });
+
+  return app.toTree();
+};
+      `);
+    }).then(() => execa('npm', ['install'], {
+        preferLocal: false,
+    }))
+  })
+
+  after(function() {
+    fixturify.writeSync('config', {
+      'environment.js': envConfigContent()
+    });
+    process.chdir(origWorkingDir);
+  }),
+
+  beforeEach(cleanFakeOpener);
+
+  it('should open rootURL', function() {
+    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
+    fixturify.writeSync('config', {
+      'environment.js': envConfigContent({
+        rootURL: '/root-url'
+      })
+    });
+
+    return runServer({
+      onChildSpawned: function () {
+        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
+          var launches = getOpenerLaunches();
+          expect(launches.length).to.equal(1, "Opener launched 1 time");
+          expect(launches[0]).to.equal('http://localhost:4200/root-url/', "Opened with rootURL");
+        });
+      }
+    });
+  });
+
+  it('should not be launched if open=false', function () {
+    console.log(process.cwd());
+    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
+    fixturify.writeSync('config', {
+      'environment.js': envConfigContent()
+    });
+
+    return runServer({
+      open: false,
+      onChildSpawned: function () {
+        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
+          var launches = getOpenerLaunches();
+          expect(launches.length).to.equal(0, "Opener not launched");
+        });
+      }
+    });
+  });
+
+  it('should open baseURL', function() {
+    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
+    fixturify.writeSync('config', {
+      'environment.js': envConfigContent({
+        baseURL: '/base-url'
+      })
+    });
+
+    return runServer({
+      onChildSpawned: function () {
+        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
+          var launches = getOpenerLaunches();
+          expect(launches.length).to.equal(1, "Opener launched 1 time");
+          expect(launches[0]).to.equal('http://localhost:4200/base-url/', "Opened with baseURL");
+        });
+      }
+    });
+  });
+
+  it('should open with startup path', function() {
+    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
+    fixturify.writeSync('config', {
+      'environment.js': envConfigContent({
+        rootURL: '/root-url'
+      })
+    });
+
+    return runServer({
+      open: 'start-up/',
+      onChildSpawned: function () {
+        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
+          var launches = getOpenerLaunches();
+          expect(launches.length).to.equal(1, "Opener launched 1 time");
+          expect(launches[0]).to.equal('http://localhost:4200/root-url/start-up/', "Opened with startup path");
+        });
+      }
+    });
+  });
+});
+
+function envConfigContent(overrides) {
   if (!overrides) {
     overrides = {};
   }
@@ -21,22 +169,6 @@ let envConfigContent = function(overrides) {
     }, ${JSON.stringify(overrides)});
   };`
 }
-
-var Promise = require("rsvp").Promise;
-var EOL = require("os").eol;
-var path = require('path');
-var fs = require('fs');
-
-var chai = require('chai');
-
-var expect = chai.expect;
-
-var runCommand = require('ember-cli-internal-test-helpers/lib/helpers/run-command');
-
-var TIME_TO_WAIT_FOR_BUILD = 10000;
-var TIME_TO_WAIT_FOR_STARTUP = 10000;
-
-var _log = [];
 
 function log(message) {
   _log.push(message);
@@ -109,100 +241,3 @@ function delay(ms) {
     setTimeout(resolve, ms);
   });
 }
-
-describe('commands/serve', function () {
-  before(function() {
-    this.timeout(300000);
-    process.chdir(targetApp);
-    return execa('bower', ['install'], {
-      preferLocal: false
-    })
-    .then(() => execa('npm', ['install'], {
-      preferLocal: false
-    }))
-  });
-
-  after(function() {
-    fixturify.writeSync('config', {
-      'environment.js': envConfigContent()
-    });
-    process.chdir(path.join('..', '..'));
-  }),
-
-  beforeEach(cleanFakeOpener);
-
-  it('should open rootURL', function() {
-    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
-    fixturify.writeSync('config', {
-      'environment.js': envConfigContent({
-        rootURL: '/root-url'
-      })
-    });
-
-    return runServer({
-      onChildSpawned: function () {
-        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
-          var launches = getOpenerLaunches();
-          expect(launches.length).to.equal(1, "Opener launched 1 time");
-          expect(launches[0]).to.equal('http://localhost:4200/root-url/', "Opened with rootURL");
-        });
-      }
-    });
-  });
-
-  it('should not be launched if open=false', function () {
-    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
-    fixturify.writeSync('config', {
-      'environment.js': envConfigContent()
-    });
-
-    return runServer({
-      open: false,
-      onChildSpawned: function () {
-        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
-          var launches = getOpenerLaunches();
-          expect(launches.length).to.equal(0, "Opener not launched");
-        });
-      }
-    });
-  });
-
-  it('should open baseURL', function() {
-    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
-    fixturify.writeSync('config', {
-      'environment.js': envConfigContent({
-        baseURL: '/base-url'
-      })
-    });
-
-    return runServer({
-      onChildSpawned: function () {
-        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
-          var launches = getOpenerLaunches();
-          expect(launches.length).to.equal(1, "Opener launched 1 time");
-          expect(launches[0]).to.equal('http://localhost:4200/base-url/', "Opened with baseURL");
-        });
-      }
-    });
-  });
-
-  it('should open with startup path', function() {
-    this.timeout(TIME_TO_WAIT_FOR_BUILD + TIME_TO_WAIT_FOR_STARTUP);
-    fixturify.writeSync('config', {
-      'environment.js': envConfigContent({
-        rootURL: '/root-url'
-      })
-    });
-
-    return runServer({
-      open: 'start-up/',
-      onChildSpawned: function () {
-        return delay(TIME_TO_WAIT_FOR_BUILD).then(function () {
-          var launches = getOpenerLaunches();
-          expect(launches.length).to.equal(1, "Opener launched 1 time");
-          expect(launches[0]).to.equal('http://localhost:4200/root-url/start-up/', "Opened with startup path");
-        });
-      }
-    });
-  });
-});
